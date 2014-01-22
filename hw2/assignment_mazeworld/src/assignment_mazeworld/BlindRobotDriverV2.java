63c8e330b4be5bd9f99f052bca67cf91c4f31f1f
jack2684 <gjj@cs.dartmouth.edu>
2014-01-22 17:46:20 -0500

    Now I know I am wrong at blind robot problem

---
+++ hw2/assignment_mazeworld/src/assignment_mazeworld/BlindRobotDriverV2.java
@@ -0,0 +1,197 @@
+package assignment_mazeworld;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import assignment_mazeworld.SearchProblem.SearchNode;
+import assignment_mazeworld.BlindRobotProblemV2.BlindRobotNode;
+import assignment_mazeworld.BlindRobotProblemV2.Coordinate;
+
+;
+
+;
+
+public class BlindRobotDriverV2 extends Application {
+
+	public class circlePlusCoord {
+		Circle cir;
+		Coordinate cd;
+
+		circlePlusCoord(Circle c, Coordinate coord) {
+			cir = c;
+			cd = coord;
+		}
+	}
+
+	Maze maze;
+
+	// instance variables used for graphical display
+	private static final int PIXELS_PER_SQUARE = 20;
+	MazeView mazeView;
+	List<AnimationPath> animationPathList;
+
+	// some basic initialization of the graphics; needs to be done before
+	// runSearches, so that the mazeView is available
+	private void initMazeView() {
+		maze = Maze.readFromFile("simple.maz");
+
+		animationPathList = new ArrayList<AnimationPath>();
+		// build the board
+		mazeView = new MazeView(maze, PIXELS_PER_SQUARE);
+
+	}
+
+	// assumes maze and mazeView instance variables are already available
+	private void runSearches() {
+
+		BlindRobotProblemV2 mazeProblem = new BlindRobotProblemV2(maze, 0, 0, 2, 2);
+
+		List<SearchNode> astarPath = mazeProblem.astarSearch();
+		animationPathList.add(new AnimationPath(mazeView, astarPath));
+		System.out.println("A*:  ");
+		mazeProblem.printStats();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	// javafx setup of main view window for mazeworld
+	@Override
+	public void start(Stage primaryStage) {
+
+		initMazeView();
+
+		primaryStage.setTitle("CS 76 Mazeworld");
+
+		// add everything to a root stackpane, and then to the main window
+		StackPane root = new StackPane();
+		root.getChildren().add(mazeView);
+		primaryStage.setScene(new Scene(root));
+
+		primaryStage.show();
+
+		// do the real work of the driver; run search tests
+		runSearches();
+
+		// sets mazeworld's game loop (a javafx Timeline)
+		Timeline timeline = new Timeline(1);
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.getKeyFrames().add(
+				new KeyFrame(Duration.seconds(.05), new GameHandler()));
+		timeline.playFromStart();
+
+	}
+
+	// every frame, this method gets called and tries to do the next move
+	// for each animationPath.
+	private class GameHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent e) {
+			// System.out.println("timer fired");
+			for (AnimationPath animationPath : animationPathList) {
+				// note: animationPath.doNextMove() does nothing if the
+				// previous animation is not complete. If previous is complete,
+				// then a new animation of a piece is started.
+				animationPath.doNextMove();
+			}
+		}
+	}
+
+	// each animation path needs to keep track of some information:
+	// the underlying search path, the "piece" object used for animation,
+	// etc.
+	private class AnimationPath {
+		private Circle piece;
+		private ArrayList<circlePlusCoord> candidates;
+		private List<SearchNode> searchPath;
+		private int currentMove = 0;
+
+		private int lastX;
+		private int lastY;
+
+		boolean animationDone = true;
+
+		public AnimationPath(MazeView mazeView, List<SearchNode> path) {
+			searchPath = path;
+			BlindRobotNode firstNode = (BlindRobotNode) searchPath.get(0);
+			candidates = new ArrayList<circlePlusCoord>();
+			for (Coordinate cd : firstNode.candidates) {
+				candidates.add(new circlePlusCoord(mazeView.addCandidate(cd.x,
+						cd.y), cd) );
+			}
+			piece = mazeView.addPiece(firstNode.getX(), firstNode.getY());
+			lastX = firstNode.getX();
+			lastY = firstNode.getY();
+		}
+
+		// try to do the next step of the animation. Do nothing if
+		// the mazeView is not ready for another step.
+		public void doNextMove() {
+
+			// animationDone is an instance variable that is updated
+			// using a callback triggered when the current animation
+			// is complete
+			if (currentMove < searchPath.size() && animationDone) {
+				BlindRobotNode mazeNode = (BlindRobotNode) searchPath
+						.get(currentMove);
+				for (circlePlusCoord cd : candidates) {
+					if (!mazeNode.inCandidates(cd.cd)) {
+						cd.cir.setVisible(false);
+					}
+					else
+						cd.cir.setVisible(true);
+				}
+				int dx = mazeNode.getX() - lastX;
+				int dy = mazeNode.getY() - lastY;
+				// System.out.println("animating " + dx + " " + dy);
+				//mazeView.footPrint(lastX, lastY, piece, (dx + 2) * 10 + dy + 2);
+				animateMove(piece, dx, dy);
+				lastX = mazeNode.getX();
+				lastY = mazeNode.getY();
+
+				currentMove++;
+			}
+
+		}
+
+		// move the piece n by dx, dy cells
+		public void animateMove(Node n, int dx, int dy) {
+			animationDone = false;
+			TranslateTransition tt = new TranslateTransition(
+					Duration.millis(100), n);
+			tt.setByX(PIXELS_PER_SQUARE * dx);
+			tt.setByY(-PIXELS_PER_SQUARE * dy);
+			// set a callback to trigger when animation is finished
+			tt.setOnFinished(new AnimationFinished());
+
+			tt.play();
+
+		}
+
+		// when the animation is finished, set an instance variable flag
+		// that is used to see if the path is ready for the next step in the
+		// animation
+		private class AnimationFinished implements EventHandler<ActionEvent> {
+			@Override
+			public void handle(ActionEvent event) {
+				animationDone = true;
+			}
+		}
+	}
+}
---
+++ hw2/assignment_mazeworld/src/assignment_mazeworld/BlindRobotProblemV2.java
@@ -0,0 +1,193 @@
+package assignment_mazeworld;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.HashSet;
+
+import assignment_mazeworld.SimpleMazeProblem.SimpleMazeNode;
+
+// Find a path for a single agent to get from a start location (xStart, yStart)
+//  to a goal location (xGoal, yGoal)
+
+public class BlindRobotProblemV2 extends InformedSearchProblem {
+
+	private static int actions[][] = { Maze.NORTH, Maze.EAST, Maze.SOUTH,
+			Maze.WEST };
+
+	private Coordinate coordStart, coordGoal;
+
+	private Maze maze;
+
+	public class Coordinate {
+		public int x;
+		public int y;
+
+		Coordinate(int a, int b) {
+			x = a;
+			y = b;
+		}
+
+		Coordinate(Coordinate other) {
+			x = other.x;
+			y = other.y;
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			return hashCode() == ((Coordinate) other).hashCode();
+		}
+
+		@Override
+		public int hashCode() {
+			return x + 10000 * y;
+		}
+	}
+
+	public BlindRobotProblemV2(Maze m, int sx, int sy, int gx, int gy) {
+		maze = m;
+		coordStart = new Coordinate(sx, sy);
+		coordGoal = new Coordinate(gx, gy);
+		startNode = new BlindRobotNode(coordStart, 0);
+	}
+
+	// node class used by searches. Searches themselves are implemented
+	// in SearchProblem.
+	public class BlindRobotNode implements SearchNode {
+
+		// location of the agent in the maze
+		protected Coordinate current, previous;
+		protected HashSet<Coordinate> candidates;
+
+		// how far the current node is from the start. Not strictly required
+		// for uninformed search, but useful information for debugging,
+		// and for comparing paths
+		private double cost;
+
+		public BlindRobotNode(Coordinate curr, double c) {
+			candidates = new HashSet<>();
+			current = new Coordinate(curr);
+			cost = c;
+
+			// initiate the candidates
+			// if it is at startnode, where previous = null
+			for (int x = 0; x < maze.width; x++) {
+				for (int y = 0; y < maze.height; y++) {
+					if (maze.getInt(x, y) == 0) {
+						// System.out.println(x + "," + y);
+						candidates.add(new Coordinate(x, y));
+					}
+				}
+			}
+		}
+
+		public BlindRobotNode(Coordinate curr, BlindRobotNode prevNode,
+				boolean noWall, double c) {
+			// initiate the positions
+			candidates = new HashSet<>();
+			current = new Coordinate(curr);
+			previous = new Coordinate(prevNode.current);
+			cost = c;
+
+			// if now is one of the searching node, compute derection first
+			int dx = current.x - previous.x, dy = current.y - previous.y;
+			for (Coordinate cd : prevNode.candidates) {
+				// if it comes from in wall node, or if the action matches
+				// we add this candidate
+				if (maze.isLegal(cd.x + dx, cd.y + dy) == noWall) {
+					Coordinate tmp = new Coordinate(cd.x + dx, cd.y + dy);
+					candidates.add(tmp);
+				}
+			}
+
+		}
+
+		public boolean inCandidates(Coordinate cd) {
+			return candidates.contains(cd);
+		}
+
+		public int getX() {
+			return current.x;
+		}
+
+		public int getY() {
+			return current.y;
+		}
+
+		public ArrayList<SearchNode> getSuccessors() {
+
+			ArrayList<SearchNode> successors = new ArrayList<SearchNode>();
+
+			for (int[] action : actions) {
+				Coordinate next = new Coordinate(current.x + action[0],
+						current.y + action[1]);
+				SearchNode succ;
+				if (maze.isLegal(next.x, next.y)) {
+					succ = new BlindRobotNode(next, this, true, getCost() + 1.0);
+				} else {
+					succ = new BlindRobotNode(current, this, true, getCost());
+				}
+				successors.add(succ);
+			}
+
+			return successors;
+
+		}
+
+		@Override
+		public boolean goalTest() {
+			// System.out.println("candidates.size(): " + candidates.size()
+			// + ". At " + getX() + "," + getY());
+			return candidates.size() == 1 && ( true || current == coordGoal);
+		}
+
+		// an equality test is required so that visited sets in searches
+		// can check for containment of states
+		@Override
+		public boolean equals(Object other) {
+			return candidates == ((BlindRobotNode) other).candidates
+					&& current == ((BlindRobotNode) other).current;
+		}
+
+		@Override
+		public int hashCode() {
+			return 1000000
+					* (10000 * candidates.size() + current.x * 1000 + current.y)
+					+ candidates.size();
+		}
+
+		@Override
+		public String toString() {
+			return new String("Maze state " + current.x + ", " + current.y
+					+ " " + " depth " + getCost());
+		}
+
+		@Override
+		public double getCost() {
+			return cost;
+		}
+
+		@Override
+		public double heuristic() {
+			// manhattan distance metric for simple maze with one agent:
+//			double dx = coordGoal.x - current.y;
+//			double dy = coordGoal.y - current.x;
+//			return Math.abs(dx) + Math.abs(dy);
+			return candidates.size() - 1;
+
+		}
+
+		@Override
+		public int compareTo(SearchNode o) {
+			return (int) Math.signum(priority() - o.priority());
+		}
+
+		@Override
+		public double priority() {
+			return heuristic() + getCost();
+		}
+
+	}
+
+}
