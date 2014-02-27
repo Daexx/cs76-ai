f0a3271864cdfe0efb0a8b51ca9262cc69f64d9e
jack2684 <gjj@cs.dartmouth.edu>
2014-02-27 16:40:28 -0500

    keep writing

--- hw5/coloring.m
+++ hw5/coloring.m
@@ -11,3 +11,13 @@ text(n-0.25,times+1,a)
 
 xlabel('Methods');
 ylabel('time (s)');
+
+nodes = [2511955 106556 5747090 51 2511955 51];
+figure();
+bar(nodes);
+set(gca,'xticklabel',{'BackTrack','MRV','LCV', 'MRV+LCV','MAC3','ALL'});
+a=num2str(nodes');
+text(n-0.3,nodes+100000,a)
+
+xlabel('Methods');
+ylabel('#nodes');
